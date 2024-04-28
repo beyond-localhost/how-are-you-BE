38a24cd42e4d4b1c6834147d12d3d6fcfc0199f5
Steven Semyung Oh <semyung.dev@gmail.com>
2024-04-28 23:32:50 +0900

    add boilerplate bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import { Elysia } from 'elysia'
+
+new Elysia()
+    .get('/', () => 'Hello Elysia')
+    .get('/user/:id', ({ params: { id }}) => id)
+    .post('/form', ({ body }) => body)
+    .listen(3000)
