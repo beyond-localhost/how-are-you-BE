2b06421d8f57d8cee2e6b1363875646ced7a8a52
Steven Semyung Oh <semyung.dev@gmail.com>
2024-06-07 12:39:01 +0900

    remove unnecessary codes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { apiReference } from "@scalar/hono-api-reference";
 
-import { Elysia, t } from "elysia";
-import { InvalidSessionError } from "./core/error";
 import { createSQLiteDatabase } from "./domain/rdb";
 
 import { resolveEnv } from "./env";
 
-import { cors as honoCors } from "hono/cors";
+import { cors } from "hono/cors";
 import auth from "./controllers/auth.controllers";
 import { depsMiddleware, honoApp } from "./runtime/hono";
 import user from "./controllers/user.controllers";
@@ -18,7 +16,7 @@ const db = createSQLiteDatabase();
 
 const app = honoApp();
 app.use(
-  honoCors({
+  cors({
     origin: ["http://localhost:5173"],
     credentials: true,
     allowHeaders: ["content-type"],
@@ -53,52 +51,3 @@ export default {
   port: env.Server.Port,
   fetch: app.fetch,
 };
-
-// .get(
-//   "/questions/answers",
-//   async ({ query, userId, set, conn }) => {
-//     const { type, startDateTime, endDateTime, offset, limit } = query;
-//     if (!startDateTime || !endDateTime) {
-//       if (type === "duration") {
-//         set.status = 400;
-//         return inputRangeError();
-//       }
-
-//       const ret = await findAllQuestionAnswersByUserId(conn, {
-//         userId,
-//         limit,
-//         offset,
-//       });
-//       const { count } = await findTotalQuestionsByUserId(conn, userId);
-//       const hasMore = count > offset + limit;
-
-//       return {
-//         data: ret.map((v) => ({
-//           questionId: v.questionDistribution.questionId,
-//           question: v.questionDistribution.question.question,
-//           answerId: v.id,
-//           answer: v.answer,
-//           createdAtUTC: v.createdAt,
-//         })),
-//         hasMore,
-//       };
-//     }
-//   },
-//   {
-//     query: t.Object({
-//       type: t.Union([t.Literal("all"), t.Literal("duration")]),
-//       startDateTime: t.Optional(t.String()),
-//       endDateTime: t.Optional(t.String()),
-//       offset: t.Number({ minimum: 1 }),
-//       limit: t.Number({ minimum: 1 }),
-//     }),
-//     response: {
-//       400: InputRangeError,
-//     },
-//     detail: {
-//       description:
-//         "type은 전체를 의미하는 all과 특정 기간을 의미하는 duration중 하나가 와야해요. duration이면 startDateTime과 endDateTime이 필수에요.",
-//       tags: ["Question"],
-//     },
-//   }
-// )
